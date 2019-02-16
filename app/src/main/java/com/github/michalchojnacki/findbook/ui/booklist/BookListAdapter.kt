569c080b5f9ca228123053dca5c248ad28593855
Michal Chojnacki <chojnackimichal@hotmail.com>
2019-02-16 16:26:32 +0100

    Styles book list item

--- app/src/main/java/com/github/michalchojnacki/findbook/ui/booklist/BookListAdapter.kt
+++ app/src/main/java/com/github/michalchojnacki/findbook/ui/booklist/BookListAdapter.kt
@@ -1,23 +1,23 @@
 package com.github.michalchojnacki.findbook.ui.booklist
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.github.michalchojnacki.findbook.R
+import com.github.michalchojnacki.findbook.databinding.BookListItemBinding
 import com.github.michalchojnacki.findbook.domain.model.Book
 
 class BookListAdapter(private val books: List<Book>) : RecyclerView.Adapter<BookListAdapter.ViewHolder>() {
-    class ViewHolder(root: View) : RecyclerView.ViewHolder(root)
+    class ViewHolder(val binding: BookListItemBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val root = LayoutInflater.from(parent.context).inflate(R.layout.book_list_item, parent, false)
-        return ViewHolder(root)
+        return ViewHolder(BookListItemBinding.bind(root))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        (holder.itemView as BookView).bind(books[position])
+        holder.binding.book = books[position]
     }
 
     override fun getItemCount() = books.size
